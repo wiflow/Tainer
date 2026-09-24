a01f0221a3c7eed8b39ca6a626412e7e6320cfd3
Nicklas Sørensen <nicklas.henri@gmail.com>
2026-09-24 08:57:55 +0200

    deps: patch vulnerable packages and drop unused runtime deps

--- src/components/ui/map.tsx
+++ src/components/ui/map.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-import MapLibreGL, { type PopupOptions, type MarkerOptions } from "maplibre-gl";
+import * as MapLibreGL from "maplibre-gl";
+import type { PopupOptions, MarkerOptions } from "maplibre-gl";
 import "maplibre-gl/dist/maplibre-gl.css";
 import {
   createContext,
